e315c6c92a0134aee8834e5332e77a33aa78ad8c
NovaIscariot <enin10@inbox.ru>
2019-11-25 14:35:35 +0300

    queue code

--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "Queue.h"
+
+template<class T>
+void Queue<T>::push(T value) {
+	const std::lock_guard<std::mutex> guard(qmutex);
+	q.push(value);
+}
+
+template<class T>
+T Queue<T>::pop() {
+	const std::lock_guard<std::mutex> guard(qmutex);
+	T top = q.front();
+	q.pop();
+	return top;
+}
+
+template<class T>
+bool Queue<T>::empty() {
+	const std::lock_guard<std::mutex> guard(qmutex);
+	return q.empty();
+}
