82879931058b363333fcff0f278961e6eddc0c7f
NovaIscariot <enin10@inbox.ru>
2019-12-06 17:13:22 +0300

    General fixes

--- General/General.cpp
+++ General/General.cpp
@@ -1,26 +1,6 @@
 #include <General.h>
 
-General::General() {
-    rqueue = std::make_shared<Queue<std::string>>();
-    pqueue = std::make_shared<Queue<std::string>>();
-    wqueue = std::make_shared<Queue<TestCase*>>();
-    db = std::make_shared<Queue<DataBase>>();
-    parsers.push_back(Parser(pqueue, wqueue, rqueue));
-    managers.push_back(Manager(wqueue, rqueue, 1));
-    reporters.push_back(Reporter(rqueue, db));
-}
-
-General::General(int cont_count) {
-    rqueue = std::make_shared<Queue<std::string>>();
-    pqueue = std::make_shared<Queue<std::string>>();
-    wqueue = std::make_shared<Queue<TestCase*>>();
-    db = std::make_shared<Queue<DataBase>>();
-    parsers.push_back(Parser(pqueue, wqueue, rqueue));
-    managers.push_back(Manager(wqueue, rqueue, cont_count));
-    reporters.push_back(Reporter(rqueue, db));
-}
-
-General::General(int p_count, int m_count, int r_count, int cont_count) {
+General::General(int cont_count, int p_count, int m_count, int r_count) {
     rqueue = std::make_shared<Queue<std::string>>();
     pqueue = std::make_shared<Queue<std::string>>();
     wqueue = std::make_shared<Queue<TestCase*>>();
@@ -36,17 +16,36 @@ General::General(int p_count, int m_count, int r_count, int cont_count) {
 void General::TurnOn() {
     for (auto ptr = parser.begin(); ptr < parsers.end(); ptr++)
     {
-        boost::thread thr(*ptr->WorkCycle());
-        thr.detach();
+        *ptr->setWorkingState(true);
+        std::thread thr(*ptr->workCycle);
+        threads.push_back(thr);
     }
     for (auto ptr = managers.begin(); ptr < managers.end(); ptr++)
     {
-        boost::thread thr(*ptr->WorkCycle());
-        thr.detach();
+        *ptr->setWorkingState(true);
+        std::thread thr(*ptr->WorkCycle);
+        threads.push_back(thr);
     }
     for (auto ptr = reporters.begin(); ptr < reporters.end(); ptr++)
     {
-        boost::thread thr(*ptr->WorkCycle());
-        thr.detach();
+        *ptr->setWorkingState(true);
+        std::thread thr(*ptr->WorkCycle);
+        threads.push_back(thr);
     }
+}
+
+void General::Turnoff() {
+    for (auto ptr = parser.begin(); ptr < parsers.end(); ptr++)
+    {
+        *ptr->setWorkingState(false);
+    }
+    for (auto ptr = managers.begin(); ptr < managers.end(); ptr++)
+    {
+        *ptr->setWorkingState(false);
+    }
+    for (auto ptr = reporters.begin(); ptr < reporters.end(); ptr++)
+    {
+        *ptr->setWorkingState(false);
+    }
+    std::for_each(threads.begin(), threads.end(), std::mem_fn(&std::thread::join()));
 }
--- General/General.h
+++ General/General.h
@@ -8,30 +8,30 @@
 #include "../TestCase/TestCase.h"
 #include "../DataBase/DataBase.h"
 
-
-#include <boost/thread/thread.hpp>
+#include <algorithm>
+#include <functional>
+#include <thread>
 #include <vector>
 #include <memory>
 #include <string>
 
 class General {
 public:
-	General();
-	General(int cont_count);
-	General(int p_count, int m_count, int r_count, int cont_count);
-	~General();
-	
-	void TurnOn();
-	void TurnOff();
-	int GetRequest(std::string request);
+    General(int cont_count = 3, int p_count = 1, int m_count = 1, int r_count = 1);
+    ~General();
+
+    void TurnOn();
+    void TurnOff();
+    int GetRequest(std::string request);
 private:
-	std::shared_ptr<Queue<std::string>> rqueue;
-	std::shared_ptr<Queue<std::string>> pqueue;
-	std::shared_ptr<Queue<TestCase*>> wqueue;
+    std::shared_ptr<Queue<std::string>> rqueue;
+    std::shared_ptr<Queue<std::string>> pqueue;
+    std::shared_ptr<Queue<TestCase*>> wqueue;
     std::shared_ptr<DataBase> db;
-	std::vector<Parser> parsers;
-	std::vector<Manager> managers;
-	std::vector<Reporter> reporters;
+    std::vector<Parser> parsers;
+    std::vector<Manager> managers;
+    std::vector<Reporter> reporters;
+    std::vector<std::thread> threads;
 }
 
 #endif //CONTINUUM_GENERAL_H
